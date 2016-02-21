302bc9d2a3dd08dafb6dc0c2f348d4d53ddf2ca6
Aaron Harvey <harvnlenny@gmail.com>
2016-02-21 10:14:55 -0500

    Added the up button to the action bar.

--- app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -1,7 +1,8 @@
 package com.hfad.bitsandpizzas;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Bundle;
 
 public class OrderActivity extends Activity {
 
@@ -9,6 +10,8 @@ public class OrderActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
 }
